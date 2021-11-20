0166ca8126a8076f4d01645a699b02a1229d3199
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-11-20 13:29:02 +0700

    feature(fishing): add fishing functionalities

---
+++ fishing.pl
@@ -0,0 +1,65 @@
+:- dynamic(player_xp/4). 
+:- dynamic(player_position/2).
+:- dynamic(fish_items/2).
+
+map_object(1,0,'o').
+map_object(1,1,'o').
+map_object(2,1,'o').
+map_object(2,2,'o').
+
+fish_list([tuna, salmon, catfist, eel, clownfish, shark]).
+fish_list_length(6).
+
+init_xp:-
+    asserta(player_xp(0,0,0,0)), !.
+
+init_pos:-
+    asserta(player_position(0,0)).
+
+startFishing:-
+    % resetFishing,
+    init_pos,
+    init_xp,
+    write('welcome!'),nl.
+
+isTiles(X,Y):-
+    map_object(X,Y,Z),
+    Z = 'o'.
+
+canFish:-
+    player_position(X,Y),
+    X1 is X - 1,
+    Y1 is Y - 1,
+    X2 is X + 1,
+    Y2 is Y + 1,
+    (isTiles(X1,Y);
+    isTiles(X2,Y);
+    isTiles(X,Y1);
+    isTiles(X,Y2)).
+
+indexOf([Head|Tail],0,Head) :- !.
+indexOf([Head|Tail],Idx,Elmt):-
+  Idx > 0,
+  Idx1 is Idx - 1,
+  indexOf(Tail, Idx1, Res),
+  Elmt = Res.
+
+takeFish :-
+    fish_list(X),
+    fish_list_length(Length),
+    random(0,Length,Idx),
+    indexOf(X,Idx,Elmt),
+    random(1,11,Lvl),
+    assertz(fish_items(Elmt,Lvl)),
+    write('You got a '),
+    write(Elmt),
+    write(' with level: '), write(Lvl), nl.
+
+cantFishMsg :-
+    write('you can not fish here').
+
+fish:-
+    canFish -> takeFish
+    ;
+    cantFishMsg.
+
